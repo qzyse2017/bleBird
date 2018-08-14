5e0b357c52e99a3ff2f9542f52fe07cb6c360ded
zyck <2902037215@qq.com>
2018-08-14 20:17:43 +0800

    Add component textEditor

--- src/app/bleBird/src/app/app.module.ts
+++ src/app/bleBird/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TextEditorComponent } from './text-editor/text-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextEditorComponent
   ],
   imports: [
     BrowserModule
